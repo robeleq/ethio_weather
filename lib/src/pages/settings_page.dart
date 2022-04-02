7bd4828ac4b86e01abeadc7535e24dab5fef8738
Robel Equbasilassie <robiki4life@gmail.com>
2022-04-02 15:03:55 +0300

    add Settings Page UI

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:weather_icons/weather_icons.dart';
+
+import '../styles/colors.dart';
+import '../widgets/settings_tile.dart';
 
 class SettingsPage extends StatefulWidget {
   final String title;
@@ -9,11 +14,147 @@ class SettingsPage extends StatefulWidget {
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
+class _SettingsPageState extends State<SettingsPage> with TickerProviderStateMixin<SettingsPage> {
+  final String _selectedLanguage = "English";
+  bool _isThemeDark = false;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(widget.title),
-    );
+    final ThemeData _theme = Theme.of(context);
+    final Color _backgroundColor = _theme.brightness == Brightness.light ? primaryLightColor : secondaryLightColor;
+    final Color _textColor = _theme.brightness == Brightness.light ? Colors.black : Colors.white;
+    final Color _subtitleColor = _theme.brightness == Brightness.light ? Colors.black54 : Colors.white60;
+
+    return SafeArea(
+        child: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Language".toUpperCase(),
+              style: _theme.textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SettingsTile(
+              title: 'Language',
+              subtitle: _selectedLanguage,
+              leading: Icon(
+                Icons.language,
+                color: _textColor,
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              "Theme".toUpperCase(),
+              style: _theme.textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SettingsTile.switchTile(
+              title: "Dark Theme",
+              leading: Icon(
+                Icons.palette,
+                color: _textColor,
+              ),
+              switchValue: _isThemeDark,
+              onToggle: (bool value) {
+                setState(() {
+                  if (!value) {
+                    _isThemeDark = false;
+                  } else {
+                    _isThemeDark = true;
+                  }
+                });
+              },
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              "Units".toUpperCase(),
+              style: _theme.textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SettingsTile(
+              title: 'Temp',
+              subtitle: "°C",
+              leading: BoxedIcon(
+                WeatherIcons.thermometer,
+                color: _textColor,
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              "About".toUpperCase(),
+              style: _theme.textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SettingsTile(
+              title: "Version",
+              leading: FaIcon(FontAwesomeIcons.codeBranch, color: _textColor),
+              subtitle: "v0.0.1",
+            ),
+            Divider(
+              color: _theme.scaffoldBackgroundColor,
+              height: 1,
+            ),
+            SettingsTile(
+              title: "Share",
+              leading: Icon(
+                Icons.share,
+                color: _textColor,
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              "Privacy".toUpperCase(),
+              style: _theme.textTheme.bodyText1?.copyWith(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            SettingsTile(
+              title: "Terms of Use",
+              leading: Icon(
+                Icons.description,
+                color: _textColor,
+              ),
+              onTap: () {},
+            ),
+            Divider(
+              color: _theme.scaffoldBackgroundColor,
+              height: 1,
+            ),
+            SettingsTile(
+              title: "Open Source Licenses",
+              leading: Icon(
+                Icons.collections_bookmark,
+                color: _textColor,
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
---
+++ lib/src/widgets/settings_tile.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import '../styles/colors.dart';
+
+enum _SettingsTileType { simple, switchTile }
+
+class SettingsTile extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final Widget? leading;
+  final VoidCallback? onTap;
+  final Function(bool value)? onToggle;
+  final bool? switchValue;
+  final _SettingsTileType _tileType;
+
+  const SettingsTile({
+    Key? key,
+    required this.title,
+    this.subtitle,
+    this.leading,
+    this.onTap,
+  })  : _tileType = _SettingsTileType.simple,
+        onToggle = null,
+        switchValue = null,
+        super(key: key);
+
+  const SettingsTile.switchTile({
+    Key? key,
+    required this.title,
+    this.subtitle,
+    this.leading,
+    required this.onToggle,
+    required this.switchValue,
+  })  : _tileType = _SettingsTileType.switchTile,
+        onTap = null,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData _theme = Theme.of(context);
+
+    final Color _backgroundColor = _theme.brightness == Brightness.light ? primaryLightColor : secondaryLightColor;
+    final Color _titleColor = _theme.brightness == Brightness.light ? Colors.black : Colors.white;
+    final Color _subtitleColor = _theme.brightness == Brightness.light ? Colors.black54 : Colors.white60;
+
+    return androidTile(_backgroundColor, _titleColor, _subtitleColor);
+  }
+
+  Widget androidTile(Color backgroundColor, Color titleColor, Color subtitleColor) {
+    if (_tileType == _SettingsTileType.switchTile) {
+      return Card(
+        elevation: 4.0,
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: backgroundColor),
+          child: SwitchListTile(
+            secondary: leading,
+            value: switchValue!,
+            onChanged: onToggle,
+            title: Text(
+              title,
+              style: TextStyle(color: titleColor),
+            ),
+            subtitle: subtitle != null ? Text(subtitle!, style: TextStyle(color: subtitleColor)) : null,
+          ),
+        ),
+      );
+    } else {
+      return Card(
+        elevation: 4.0,
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: backgroundColor),
+          child: ListTile(
+            title: Text(title, style: TextStyle(color: titleColor)),
+            subtitle: subtitle != null ? Text(subtitle!, style: TextStyle(color: subtitleColor)) : null,
+            leading: leading,
+            onTap: onTap,
+          ),
+        ),
+      );
+    }
+  }
+}
